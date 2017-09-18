73587f9936296007e628775896e7bd6981c1c273
Abigail <abi25.fly@gmail.com>
2017-09-18 11:08:45 -0500

    falta js

---
+++ js/main.js
@@ -0,0 +1,37 @@
+var preguntas= [
+  {
+    pregunta:"¿Cuál es la aerolínea más antigua del mundo?",
+    opcion1: "KLM",
+    opcion2: "Avianca",
+    opcion3: "Qantas",
+    respuesta: "KLM"
+  },
+  {
+    pregunta:"¿Cuál es el puerto más grande del mundo?",
+    opcion1: "Puerto de Singapur",
+    opcion2: "Puerto de Shangai",
+    opcion3: "Puerto de Rotterdam",
+    respuesta: "Puerto de Shangai"
+  },
+  {
+    pregunta:"¿Cuál es la distancia más larga que recorre una bicicleta hacia atrás?",
+    opcion1: "89.9 Km",
+    opcion2: "570.5 Km",
+    opcion3: "337.60 km",
+    respuesta: "337.60 km"
+  },
+  {
+    pregunta:"¿Cuál es la velociad más alta alcanzada por un bus escolar?",
+    opcion1: "590 km/h",
+    opcion2: "320 Km/h",
+    opcion3: "245 Km/h",
+    respuesta: "590 km/h"
+  },
+  {
+    pregunta:"¿Cuál es el viaje más largo en carro con un tanque de gas?",
+    opcion1: "3568 Km",
+    opcion2: "2617 km",
+    opcion3: "1732 Km",
+    respuesta: "2617 km"
+  }
+]
